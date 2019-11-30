a68d84c3d2a380b2ee0c56d3b566c04a742307b2
NhanNT <nhannt.bk@gmail.com>
2019-12-01 00:25:45 +0900

    Add node server for frontend

---
+++ frontend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+  return res.send('pong');
+});
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+console.log("FrontEnd UI run on localhost:3000");
+
+app.listen(process.env.PORT || 3000);
