97e1871dd18f2e54d7ac99fda1df12a415de6bf6
NhanNT <nguyen.thanh.nhan@moneyforward.co.jp>
2019-12-05 12:40:38 +0900

    Change basic layout

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,47 +1,35 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Layout, Menu, Icon, Button} from 'antd';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import i18n from '../../config/i18n';
+import Logo from '../../assets/images/logo.png';
 import './Layout.scss';
 
-const {Header, Sider, Content} = Layout;
+const {Header, Content} = Layout;
 
 const MainLayout = ({children}) => {
 
-  const [collapsed, setCollapsed] = useState(false);
-
-  const toggle = () => setCollapsed(!collapsed);
 
   return (
     <Layout id="app-bar">
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1">
-            <Link to="/">
-              <Icon type="user" />
-              <span>{i18n.t('sideMenu.homepage')}</span>
-            </Link>
-          </Menu.Item>
-        </Menu>
-      </Sider>
       <Layout>
-        <Header className="header" style={{display: 'flex', flexDirection: 'row'}}>
-          <div className="trigger">
-            <Icon
-              type={collapsed ? 'menu-unfold' : 'menu-fold'}
-              onClick={toggle}
-            />
+        <Header className="header">
+          <div className="container" style={{display: 'flex', flexDirection: 'row'}}>
+            <div className="logo">
+              <Link to="/">
+                <img src={Logo} alt="Logo" />
+                <span>DM2M</span>
+              </Link>
+            </div>
+            <div className="vertical-align">
+              <Button>{i18n.t('common.login')}</Button>
+            </div>
           </div>
-          <div className="vertical-align">
-            <Button>{i18n.t('common.login')}</Button>
-          </div>
+
         </Header>
-        <Content
-          className="main-content"
-        >
+        <Content className="container main-content" style={{marginTop: '1rem'}}>
           {children}
         </Content>
       </Layout>
