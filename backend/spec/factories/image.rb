4774105f91fef991d0d1896d0ea95e43bea928b6
NhanNT <nguyen.thanh.nhan@moneyforward.co.jp>
2019-12-18 12:28:37 +0900

    - Rename type column in building table - Replace association with trait in factories rspec

--- backend/spec/factories/image.rb
+++ backend/spec/factories/image.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :image do
     url { Faker::Internet.url }
     description { Faker::Lorem.word }
-    association(:room)
     trait :with_room do
       after :build do |image|
         image.room = FactoryBot.build :room
