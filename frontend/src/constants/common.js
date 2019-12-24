1c32d3130f8c0913c0bd9ef7232209b12f3581d1
NhanNT <nguyen.thanh.nhan@moneyforward.co.jp>
2019-12-24 14:09:35 +0900

    Add nearest filter option for building list at homepage

--- frontend/src/constants/common.js
+++ frontend/src/constants/common.js
@@ -3,41 +3,45 @@ import i18n from '../config/i18n';
 export const SORT_OPTIONS = {
   cheapest: {
     key: 'cheapest',
-    value: i18n.t('homepage.sort_options.cheapest_fee')
+    value: i18n.t('homepage.sort_options.cheapest_fee'),
   },
   most_expensive: {
     key: 'most_expensive',
-    value: i18n.t('homepage.sort_options.most_expensive_fee')
+    value: i18n.t('homepage.sort_options.most_expensive_fee'),
   },
   recommended: {
     key: 'recommended',
-    value: i18n.t('homepage.sort_options.recommended')
+    value: i18n.t('homepage.sort_options.recommended'),
   },
   newest_building: {
     key: 'newly_built',
-    value: i18n.t('homepage.sort_options.newest_building')
+    value: i18n.t('homepage.sort_options.newest_building'),
   },
   most_largest: {
     key: 'largest',
-    value: i18n.t('homepage.sort_options.most_largest')
-  }
+    value: i18n.t('homepage.sort_options.most_largest'),
+  },
+  nearest: {
+    key: 'nearest',
+    value: i18n.t('homepage.sort_options.nearest'),
+  },
 };
 
 export const NUMBER_OF_ITEMS = [
   {
     key: 10,
-    value: i18n.t('homepage.number_items', {count: 10})
+    value: i18n.t('homepage.number_items', {count: 10}),
   },
   {
     key: 15,
-    value: i18n.t('homepage.number_items', {count: 15})
+    value: i18n.t('homepage.number_items', {count: 15}),
   },
   {
     key: 20,
-    value: i18n.t('homepage.number_items', {count: 20})
+    value: i18n.t('homepage.number_items', {count: 20}),
   },
   {
     key: 30,
-    value: i18n.t('homepage.number_items', {count: 30})
+    value: i18n.t('homepage.number_items', {count: 30}),
   },
 ];
